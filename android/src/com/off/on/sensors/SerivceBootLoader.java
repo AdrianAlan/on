9ab39ef76b658003ed44c7c8ac37d77979331693
aa <adrianalan.pol@gmail.com>
2013-10-16 20:25:48 +0800

    Renamed

---
+++ android/src/com/off/on/sensors/SerivceBootLoader.java
@@ -0,0 +1,28 @@
+package com.off.on.sensors;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SerivceBootLoader extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context cxt, Intent receiverIntent) {
+		 if(receiverIntent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED))
+		   {         
+			 getCurrentState(true, true, cxt);
+		   }
+	}
+	
+	private void getCurrentState(boolean locationService,
+			boolean orientationService, Context cxt) {
+		if (locationService) {
+			cxt.startService(new Intent(cxt,
+					LocationService.class));
+		}
+		if (orientationService) {
+			cxt.startService(new Intent(cxt,
+					OrientationService.class));
+		}
+	}
+}
